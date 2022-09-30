6c86fe9393afb1934074346b19822b3e67f7d413
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-30 16:30:38 +0500

    Git Init

---
+++ Exercise-20/index.js
@@ -0,0 +1,7 @@
+let countries = ["Iran", "Netherlands", "Russia", "Turkey", "Canada"];
+console.log(countries);
+
+for (var index = 0; index < countries.length; index++) {
+    var items = countries[index];
+    console.log(items);
+}
